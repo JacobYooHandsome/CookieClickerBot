4d09a2f59658e586c41bf4d2d00f8d88d00719b4
Jacob Yoo <tolearneverything123@gmail.com>
2023-08-08 17:00:04 -0400

    Refactor to OOP and add save and import functions

---
+++ driver.py
@@ -0,0 +1,58 @@
+import os
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+os.environ['PATH'] += r'/Users/jacobyoo/Desktop/PythonProjects/chromedriver'
+
+class Driver(webdriver.Chrome):
+    def __init__(self):
+        super().__init__()
+        self.get("https://orteil.dashnet.org/cookieclicker/")
+        time.sleep(3)
+        self.find_element(By.ID, "langSelect-EN").click()
+        time.sleep(3)
+        self.cookie_btn = self.find_element(By.ID, "bigCookie")
+        self.upgrade = None
+        self.bakers = []
+        for i in range(20):
+            self.bakers.append(self.find_element(By.ID, f"product{i}"))
+    
+    def check_upgrade(self):
+        try:
+            self.upgrade = self.find_element(By.ID, "upgrade0")
+            classes = self.upgrade.get_attribute("class").split()
+            if "enabled" in classes:
+                self.upgrade.click()
+                self.upgrade = self.find_element(By.ID, "upgrade0")
+        except NoSuchElementException:
+            pass
+        except Exception as e:
+            print("Error:", e)
+            raise Exception("An unexpected error occured")
+    
+    def check_hiring(self):
+        for baker in self.bakers:
+            classes = baker.get_attribute("class").split()
+            if "enabled" in classes:
+                baker.click()
+                break
+    
+    def export_save(self):
+        self.find_element(By.XPATH, '//*[@id="prefsButton"]/div').click()
+        self.find_element(By.XPATH, '//*[@id="menu"]/div[3]/div/div[4]/a[1]').click()
+        with open("game_save.txt", "w") as file:
+            file.write(self.find_element(By.ID, "textareaPrompt").text)
+    
+    def import_save(self):
+        with open("game_save.txt", "r") as file:
+            content = file.read()
+            if content == "":
+                print("Sorry but you have no saved game.")
+            else:
+                self.find_element(By.XPATH, '//*[@id="prefsButton"]/div').click()
+                self.find_element(By.XPATH, '//*[@id="menu"]/div[3]/div/div[4]/a[2]').click()
+                self.find_element(By.ID, "textareaPrompt").send_keys(content + "\n")
+                self.find_element(By.XPATH, '//*[@id="prefsButton"]/div').click()
+        
--- main.py
+++ main.py
@@ -1,58 +1,28 @@
-import os
-from selenium import webdriver
 import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from driver import Driver
 
-os.environ['PATH'] += r'/Users/jacobyoo/Desktop/PythonProjects/chromedriver'
-driver = webdriver.Chrome()
+driver = Driver()
 
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+if input("Start a saved game? (y/n): \n").lower() == "y":
+    driver.import_save()
 
-time.sleep(3)
-driver.find_element(By.ID, "langSelect-EN").click()
-time.sleep(3)
-
-cookie_btn = driver.find_element(By.ID, "bigCookie")
-
-timeout = time.time() + 60 * 5
-
+minutes = int(input("How many minutes do you want to run this game for?: \n"))
+timeout = time.time() + 60 * minutes
 time_check = time.time() + 1
-
-bakers = []
-for i in range(20):
-    bakers.append(driver.find_element(By.ID, f"product{i}"))
-
-upgrade = None
+time_check_2 = time.time() + 5
 
 while True:
     if time.time() > timeout:
         break
-    cookie_btn.click()
     
+    driver.cookie_btn.click()
+
     if time.time() > time_check:
         time_check += 1
         
-        if upgrade == None:
-            try:
-                upgrade = driver.find_element(By.ID, "upgrade0")
-            except:
-                pass
-        else:
-            upgrade = driver.find_element(By.ID, "upgrade0")
-            classes = upgrade.get_attribute("class").split()
-            if "enabled" in classes:
-                upgrade.click()
-                upgrade = driver.find_element(By.ID, "upgrade0")
-        
-        for baker in bakers:
-            classes = baker.get_attribute("class").split()
-            if "enabled" in classes:
-                baker.click()
-        
-        
-                
-            
-    
+        driver.check_upgrade()
+        driver.check_hiring()
+
+driver.export_save()
 
 driver.quit()
