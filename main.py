cdb18eebf2111fc0d011f4812cdeff823685093b
Jacob Yoo <tolearneverything123@gmail.com>
2023-08-08 15:03:53 -0400

    Add basic upgrade functionality

--- main.py
+++ main.py
@@ -15,8 +15,44 @@ time.sleep(3)
 
 cookie_btn = driver.find_element(By.ID, "bigCookie")
 
-t_end = time.time() + 60 * 5
-while time.time() < t_end:
+timeout = time.time() + 60 * 5
+
+time_check = time.time() + 1
+
+bakers = []
+for i in range(20):
+    bakers.append(driver.find_element(By.ID, f"product{i}"))
+
+upgrade = None
+
+while True:
+    if time.time() > timeout:
+        break
     cookie_btn.click()
+    
+    if time.time() > time_check:
+        time_check += 1
+        
+        if upgrade == None:
+            try:
+                upgrade = driver.find_element(By.ID, "upgrade0")
+            except:
+                pass
+        else:
+            upgrade = driver.find_element(By.ID, "upgrade0")
+            classes = upgrade.get_attribute("class").split()
+            if "enabled" in classes:
+                upgrade.click()
+                upgrade = driver.find_element(By.ID, "upgrade0")
+        
+        for baker in bakers:
+            classes = baker.get_attribute("class").split()
+            if "enabled" in classes:
+                baker.click()
+        
+        
+                
+            
+    
 
 driver.quit()
